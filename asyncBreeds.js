3ffb39024a1f4ad4f111e0c05122bad974f033d7
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-21 14:34:29 -0400

    adding async breeds.js

---
+++ asyncBreeds.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) functionToRunWhenThingsAreDone(data);
+  });
+};
+
+const printOutCatBreed = breed => {
+  console.log('Return Value: ', breed);
+};
+
+breedDetailsFromFile('Bombay', printOutCatBreed);
